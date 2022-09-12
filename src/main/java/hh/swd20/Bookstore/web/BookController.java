9a94397be8deab21713f355c76a9293c077cfe58
Cisse Baker <baker.cisse@gmail.com>
2022-09-12 10:28:04 +0300

    added ResponseBody to BookContoller

--- src/main/java/hh/swd20/Bookstore/web/BookController.java
+++ src/main/java/hh/swd20/Bookstore/web/BookController.java
@@ -3,13 +3,15 @@ package hh.swd20.Bookstore.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@ResponseBody
 public class BookController {
 
 	@RequestMapping(value = "/index", method = RequestMethod.GET)
 	public String getBooks() {
-		return null;
+		return "No Books here yet...";
 		
 	}
 }
