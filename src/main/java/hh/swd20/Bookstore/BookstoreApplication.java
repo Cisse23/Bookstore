42d3e230456e2cadd6b846431b85ca5323f31776
Cisse Baker <baker.cisse@gmail.com>
2022-09-27 22:18:51 +0300

    C4.1 Category valmis

--- src/main/java/hh/swd20/Bookstore/BookstoreApplication.java
+++ src/main/java/hh/swd20/Bookstore/BookstoreApplication.java
@@ -2,6 +2,8 @@ package hh.swd20.Bookstore;
 
 import hh.swd20.Bookstore.domain.Book;
 import hh.swd20.Bookstore.domain.BookRepository;
+import hh.swd20.Bookstore.domain.Category;
+import hh.swd20.Bookstore.domain.CategoryRepository;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,17 +24,29 @@ public class BookstoreApplication {
 	
 //  testidatan luonti H2-testitietokantaan aina sovelluksen käynnistyessä
 	@Bean
-	public CommandLineRunner bookTest(BookRepository bookRepository) { 
+	public CommandLineRunner bookTest(BookRepository bookRepository, CategoryRepository catRepository) { 
 		return (args) -> {
 			log.info("save a couple of books");
 			Book book1 = new Book("Pride and Prejudice", "Jane Austen", 2012, "9780141199078", 9.20 );
 			Book book2 = new Book("The Pillars of the Earth", "Ken Follett", 2017, "9781509848492", 21.95);
 			bookRepository.save(book1);
 			bookRepository.save(book2);	
+			log.info("save some categories");
+			Category cat1 = new Category("Satire");
+			Category cat2 = new Category("Adventure");
+			Category cat3 = new Category("Biography");
+			catRepository.save(cat1);
+			catRepository.save(cat2);
+			catRepository.save(cat3);
 			
 			log.info("fetch all books");
 			for (Book b : bookRepository.findAll()) {
 				log.info(b.toString());
+			
+			log.info("fetch all categories");
+			for (Category c : catRepository.findAll()) {
+				log.info(c.toString());
+			}
 			}
 		};
 	}
