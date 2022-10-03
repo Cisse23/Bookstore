2f02c4ae1ee71fd472b1c10d8a3e08bbc160c0e2
Cisse Baker <baker.cisse@gmail.com>
2022-10-03 11:50:38 +0300

    5.1. Customized Spring REST valmis

--- src/main/java/hh/swd20/Bookstore/web/BookController.java
+++ src/main/java/hh/swd20/Bookstore/web/BookController.java
@@ -1,5 +1,8 @@
 package hh.swd20.Bookstore.web;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import hh.swd20.Bookstore.domain.Book;
 import hh.swd20.Bookstore.domain.BookRepository;
@@ -26,6 +30,18 @@ public class BookController {
 	public String bookList(Model model) {
 		model.addAttribute("books", bookRepository.findAll());
 		return "booklist";
+	}
+	
+	//REST get all books json
+	@RequestMapping(value="/books", method = RequestMethod.GET)
+	public @ResponseBody List<Book> bookListRest(){
+		return (List<Book>) bookRepository.findAll();
+	}
+	
+	//REST get book by id json
+	@RequestMapping(value="/books/{id}", method = RequestMethod.GET)
+	public @ResponseBody Optional<Book> findBookById(@PathVariable("id") Long bookId) {
+		return bookRepository.findById(bookId);
 	}
 	
 	@RequestMapping(value = "/add")
